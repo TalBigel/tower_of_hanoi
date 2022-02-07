523f5eaeec0f0fa484afb2f5068ffa2c57fbda8a
Tal Bigel <tal.bigel@gmail.com>
2022-02-07 21:39:37 +0200

    A simple dumb tower of hanoi puzzle impl. constraint to 3 rings

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+TOWERS_FORMAT = "{0:1}\t{1:1}\t{2:1}"
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def print_board(game_state):
+    for i in range(0, 3):
+        print(TOWERS_FORMAT.format(game_state["a"][i], game_state["b"][i], game_state["c"][i]))
+    print(TOWERS_FORMAT.format("a", "b", "c"))
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    game_won = False
+    game_state = {"a": [3, 2, 1], "b": [0, 0, 0], "c": [0, 0, 0]}
+    while True:
+        print_board(game_state)
+        source = input("Choose source: ")
+        print(source)
+        target = input("Choose target: ")
+        print(target)
+        if source not in game_state or target not in game_state:
+            print("Invalid values. Can only use 'a', 'b' and 'c'")
+            continue
+        if source == target:
+            print("Can't choose the same source and target")
+            continue
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        rings_source = game_state.get(source)
+        rings_target = game_state.get(target)
+        max_source = max(rings_source)
+        max_target = max(rings_target)
+        if max_source < max_target:
+            print("The target tower has a bigger ring than the source tower")
+            continue
+
+        rings_source[rings_source.index(max_source)] = 0
+        rings_target[max([i for i, x in enumerate(rings_target) if x == 0])] = max_source
+
+        if list(range(3, 0, -1)) == game_state.get("c"):
+            break
+
+    print("You won!")
